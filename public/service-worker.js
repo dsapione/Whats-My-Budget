378700c4b726858a6175a54d6340ddc9a495cc0f
Lewis <dsapione@gmail.com>
2022-04-09 20:13:28 -0400

    added service worker script to index, and file paths to service worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,17 +2,10 @@ const APP_PREFIX = 'BudgetTrack';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-	// "./index.html",
-	// "./events.html",
-	// "./tickets.html",
-	// "./schedule.html",
-	// "./assets/css/style.css",
-	// "./assets/css/bootstrap.css",
-	// "./assets/css/tickets.css",
-	// "./dist/app.bundle.js",
-	// "./dist/events.bundle.js",
-	// "./dist/tickets.bundle.js",
-	// "./dist/schedule.bundle.js"
+	"./index.html",
+	"./css/style.css",
+	"./index.js",
+	"./idb.js"
 ];
 
 self.addEventListener('install', function (e) {
